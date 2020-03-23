1b85bc047f5ee54516fa591468094e3963515382
OMHavec <59944164+OMHavec@users.noreply.github.com>
2020-03-23 18:33:20 -0400

    created PrintCommand class

---
+++ src/PrintCommand.java
@@ -0,0 +1,15 @@
+public class PrintCommand implements Command {
+	
+	private Document doc;
+
+	public PrintCommand(Document doc) {
+		this.doc = doc;
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
