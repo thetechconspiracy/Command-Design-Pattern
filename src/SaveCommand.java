1fb39df43b96583fe65b7c04d6360620a8d4ecf9
thetechconspiracy <thetechconspiracy@outlook.com>
2020-03-23 18:48:31 -0400

    SaveCommand done

--- src/SaveCommand.java
+++ src/SaveCommand.java
@@ -3,5 +3,17 @@
  * Description
  */
 
-public class SaveCommand {
+public class SaveCommand implements Command {
+
+  private Document document;
+
+  /**
+   *
+   * @param document: The document that the method operates on.
+   */
+  public SaveCommand(Document document) { this.document = document; }
+  /**
+   *Calls the Save command in the document
+   */
+  public void execute() { document.save(); }
 }
