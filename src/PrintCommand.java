7af366db5582c9432175c6ea91b54721da499085
OMHavec <59944164+OMHavec@users.noreply.github.com>
2020-03-23 18:38:12 -0400

    finalized PrintCommand

--- src/PrintCommand.java
+++ src/PrintCommand.java
@@ -1,15 +1,25 @@
+/**
+ * @author Colin Bowes
+ * executes print() method from Document class
+ */
 public class PrintCommand implements Command {
 	
 	private Document doc;
 
+	/**
+	 * @param doc  --	the document to be printed
+	 * establishes the document
+	 */
 	public PrintCommand(Document doc) {
 		this.doc = doc;
 	}
 
+	/**
+	 * calls the document's print() method which displays the print message
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-
+		doc.print();
 	}
 
 }
